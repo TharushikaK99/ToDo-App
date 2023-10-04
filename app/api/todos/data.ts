d948b98cb1c59c826b6b97e7f1bff7dfc96d54e8
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-10-05 00:06:10 +0530

    Add GET function for getting todos in backend.

---
+++ app/api/todos/data.ts
@@ -0,0 +1,7 @@
+const todos = [
+    { id: 1, name: "First One", isComplete: false},
+    { id: 2, name: "Second One", isComplete: true},
+    { id: 3, name: "Third One", isComplete: false},
+]
+
+export default todos;
