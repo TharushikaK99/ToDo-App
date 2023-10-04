da6948bbfee97ab07312f50ec6441a1dc3a7a75a
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-10-04 21:08:48 +0530

    Add input box and button in input file.

---
+++ app/components/input.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
+const Input = () => {
+    const [ value, setValue ] = useState("");
+
+    return ( 
+        <div className="w-full flex justify-center gap-5 mb-12">
+            <input 
+                type="text" 
+                name="todo" 
+                className="w-68 justify-center border-b border-gray-100 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-200 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-96  lg:rounded-xl lg:border lg:bg-gray-200 lg:px-4 lg:py-3 lg:dark:bg-zinc-800/30"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+            />
+            <button 
+                onClick={() => console.log("clicked!")} 
+                className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 dark:hover:border-gray-200"
+            >
+                Add
+            </button>
+        </div>
+     );
+}
+ 
+export default Input;
