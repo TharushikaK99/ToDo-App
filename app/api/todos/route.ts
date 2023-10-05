0f10039096996fa27cdeb08f7eea7de11a0a3eb7
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-10-05 19:51:21 +0530

    Add post route handler in backend.

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import todos from "./data";
+import todos, { addTodo } from "./data";
 
 export function GET(req: NextRequest) {
     return NextResponse.json(todos, { status: 200 });
+}
+
+export async function POST(req: NextRequest) {
+    const { name } = await req.json();
+    const todo = addTodo(name);
+    return NextResponse.json(todo, { status: 201 });
 }
