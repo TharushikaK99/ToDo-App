f7109b716b88ce8f08a0ca7f832e6adc779cb76d
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-10-05 14:18:37 +0530

    Add delete and update route handlers in route file.

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -1,12 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
-import todos from "../data";
+import { deleteTodo, getTodos, updateTodo } from "../data";
 
 interface Props {
     params: { id: number}
 }
 
 export function GET(req: NextRequest, { params: { id }}: Props) {
+    const todos = getTodos();
     const todo = todos.find(t => t.id == id);
     if (!todo) return NextResponse.json({ error: "Invalid id!"}, { status: 400 });
     return NextResponse.json(todo);
+}
+
+export function DELETE(req: NextRequest, { params: { id }}: Props) {
+    const todo = deleteTodo(id);
+    if (!todo) return NextResponse.json({ error: "Invalid id!" }, { status: 400 });
+    return NextResponse.json(todo, { status: 200 });
+}
+
+export async function PUT(req: NextRequest, { params: { id }}: Props) {
+    const { name, isComplete } = await req.json();
+    const todo = updateTodo({ id, name, isComplete });
+    if (!todo) return NextResponse.json({ error: "Invalid id!" }, { status: 400 });
+    return NextResponse.json(todo, { status: 200 });
 }
